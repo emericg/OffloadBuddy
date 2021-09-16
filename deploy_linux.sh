dfb261ff7afc07ca2a6dca1274a5e2b4bd053600
Emeric <emeric.grange@gmail.com>
2021-09-16 13:05:35 +0200

    Version is now 0.9

--- deploy_linux.sh
+++ deploy_linux.sh
@@ -3,7 +3,7 @@
 echo "> OffloadBuddy packager (Linux x86_64)"
 
 export APP_NAME="OffloadBuddy";
-export APP_VERSION=0.8;
+export APP_VERSION=0.9;
 export GIT_VERSION=$(git rev-parse --short HEAD);
 
 ## CHECKS ######################################################################
